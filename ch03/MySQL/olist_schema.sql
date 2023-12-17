8bd266f43530b55c90643b8e80bb4354092e6afe
Alkin Tezuysal <alkin.tezuysal@gmail.com>
2023-12-17 14:52:21 +0300

    Olist db raw schema
    Signed-off-by: Alkin Tezuysal <alkin.tezuysal@gmail.com>

---
+++ ch03/MySQL/olist_schema.sql
@@ -0,0 +1,188 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for macos10.15 (x86_64)
+--
+-- Host: 127.0.0.1    Database: olist
+-- ------------------------------------------------------
+-- Server version	8.0.35
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `customers`
+--
+
+DROP TABLE IF EXISTS `customers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customers` (
+  `customer_id` varchar(45) NOT NULL,
+  `customer_unique_id` varchar(45) NOT NULL,
+  `customer_zip_code_prefix` int DEFAULT NULL,
+  `customer_city` varchar(25) DEFAULT NULL,
+  `customer_state` char(2) DEFAULT NULL,
+  PRIMARY KEY (`customer_id`),
+  UNIQUE KEY `customer_id_UNIQUE` (`customer_id`),
+  UNIQUE KEY `customer_unique_id_UNIQUE` (`customer_unique_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `geolocation`
+--
+
+DROP TABLE IF EXISTS `geolocation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `geolocation` (
+  `geolocation_zip_code_prefix` int NOT NULL,
+  `geolocation_lat` decimal(9,6) DEFAULT NULL,
+  `geolocation_lng` decimal(9,6) DEFAULT NULL,
+  `geolocation_city` varchar(45) DEFAULT NULL,
+  `geolocation_state` char(2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `order_items`
+--
+
+DROP TABLE IF EXISTS `order_items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `order_items` (
+  `order_id` varchar(32) NOT NULL,
+  `order_item_id` int DEFAULT NULL,
+  `product_id` varchar(45) DEFAULT NULL,
+  `seller_id` varchar(45) DEFAULT NULL,
+  `shipping_limit_date` datetime DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `freight_value` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `order_payments`
+--
+
+DROP TABLE IF EXISTS `order_payments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `order_payments` (
+  `order_id` varchar(45) NOT NULL,
+  `payment_sequential` int DEFAULT NULL,
+  `payment_type` varchar(15) DEFAULT NULL,
+  `payment_installments` int DEFAULT NULL,
+  `payment_value` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `order_reviews`
+--
+
+DROP TABLE IF EXISTS `order_reviews`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `order_reviews` (
+  `review_id` varchar(45) NOT NULL,
+  `order_id` varchar(45) DEFAULT NULL,
+  `review_score` int DEFAULT NULL,
+  `review_comment_title` varchar(50) DEFAULT NULL,
+  `review_comment_message` varchar(250) DEFAULT NULL,
+  `review_creation_date` datetime DEFAULT NULL,
+  `review_answer_timestamp` datetime DEFAULT NULL,
+  PRIMARY KEY (`review_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `order_id` varchar(45) NOT NULL,
+  `customer_id` varchar(45) DEFAULT NULL,
+  `order_status` varchar(15) DEFAULT NULL,
+  `order_purchase_timestamp` datetime DEFAULT NULL,
+  `order_approved_at` datetime DEFAULT NULL,
+  `order_delivered_carrier_date` datetime DEFAULT NULL,
+  `order_delivered_customer_date` datetime DEFAULT NULL,
+  `order_estimated_delivery_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `product_cateegory_name_translation`
+--
+
+DROP TABLE IF EXISTS `product_cateegory_name_translation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product_cateegory_name_translation` (
+  `product_category_name` varchar(45) NOT NULL,
+  `product_cateegory_name_english` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`product_category_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `products` (
+  `product_id` varchar(45) NOT NULL,
+  `product_category_name` varchar(60) DEFAULT NULL,
+  `product_name_lenght` int DEFAULT NULL,
+  `product_description_lenght` int DEFAULT NULL,
+  `product_photos_qty` int DEFAULT NULL,
+  `product_weight_g` decimal(10,3) DEFAULT NULL,
+  `product_length_cm` decimal(10,0) DEFAULT NULL,
+  `product_height_cm` decimal(10,0) DEFAULT NULL,
+  `product_width_cm` decimal(10,0) DEFAULT NULL,
+  PRIMARY KEY (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `sellers`
+--
+
+DROP TABLE IF EXISTS `sellers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sellers` (
+  `seller_id` varchar(45) NOT NULL,
+  `seller_zip_code_prefix` decimal(5,0) DEFAULT NULL,
+  `seller_city` varchar(45) DEFAULT NULL,
+  `seller_state` char(2) DEFAULT NULL,
+  PRIMARY KEY (`seller_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-12-17 14:51:28
