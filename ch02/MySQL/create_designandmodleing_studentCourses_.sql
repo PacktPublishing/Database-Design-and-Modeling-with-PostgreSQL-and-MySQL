bbb97d627c104d194ce5d91a2de12da6d1e028e3
Alkin Tezuysal <alkin.tezuysal@gmail.com>
2024-03-06 14:19:48 +0300

    Corrections and typos
    Signed-off-by: Alkin Tezuysal <alkin.tezuysal@gmail.com>

--- ch02/MySQL/create_designandmodleing_studentCourses_.sql
+++ ch02/MySQL/create_designandmodleing_studentCourses_.sql
@@ -2,10 +2,9 @@
 
 CREATE SCHEMA IF NOT EXISTS `DesignAndModeling` DEFAULT CHARACTER SET utf8mb4; 
 
-CREATE TABLE IF NOT EXISTS `DesignandModeling`.`Student Courses` ( 
-  `StudentID` INT NOT NULL AUTO_INCREMENT, 
+CREATE TABLE IF NOT EXISTS `DesignAndModeling`.`StudentCourses` ( 
+  `StudentID` INT UNSIGNED NOT NULL AUTO_INCREMENT, 
   `StudentNAME` VARCHAR(45) NULL, 
   `Courses` VARCHAR(45) NULL, 
-  PRIMARY KEY (`StudentID`), 
-  UNIQUE INDEX `StudentID_UNIQUE` (`StudentID` ASC) VISIBLE) 
+  PRIMARY KEY (`StudentID`)) 
 ENGINE = InnoDB; 
